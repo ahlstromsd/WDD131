079b07c5189f6c63e798c207d6211ed2ae545758
Sydney Ahlstrom <ahl19001@byui.edu>
2024-10-14 22:48:25 -0600

    part 1

---
+++ coolpics/coolpics.js
@@ -0,0 +1,4 @@
+document.getElementById('menu').addEventListener('click', () => {
+    const nav = document.querySelector('nav');
+    nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
+});
